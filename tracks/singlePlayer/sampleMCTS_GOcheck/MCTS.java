546ce619febd54c89b0cc3997d47d1325d320c38
ehauckdo <ehauckdo@uwaterloo.ca>
2017-06-19 20:00:08 -0300

    Whoops, forgot function to print average

--- tracks/singlePlayer/sampleMCTS_GOcheck/MCTS.java
+++ tracks/singlePlayer/sampleMCTS_GOcheck/MCTS.java
@@ -67,5 +67,17 @@ public class MCTS
         return action;
     }
     
+    public void printRolloutsAverage(){
+        int average = 0;
+        if(rolloutsPerAct.size() > 0){
+            int sum = 0;
+            for(Integer i: rolloutsPerAct){
+                sum += i;
+            }
+            average = sum/rolloutsPerAct.size();   
+        }
+        System.out.println("Average: "+average);
+    }
+    
 
 }
